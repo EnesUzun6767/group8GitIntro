9c96ad87ee7825ec4c2edb619e72fbd5541ec51c
enes <enesuzun2020@gmail.com>
2022-02-09 10:00:07 -0500

    hw is done

--- src/TC001.java
+++ src/TC001.java
@@ -1,3 +1,17 @@
 public class TC001 {
+    public static void main(String[] args) {
+       int num1=43;
+       String result=oddEven(num1);
+        System.out.println("result = " + result);
 
+    }
+    public static String oddEven(int num){
+        String result="";
+        if(num%2==0){
+           result="even";
+        }else{
+            result="odd";
+        }
+        return result;
+    }
 }
